8c327734d654253e5e6e3a262cf07c43385c7b75
ruijiewang <ruijie_w@hotmail.com>
2016-03-24 00:03:25 -0400

    integrate new latest package

---
+++ New Unity Project/Assets/Ultimate Isometric Toolkit/Editor/reminderHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+[InitializeOnLoad]
+public class reminderHelper  {
+
+	static reminderHelper()
+     {
+         EditorApplication.update += reminder;
+     }
+     static void reminder()
+     {
+         EditorApplication.update -= reminder;
+
+		 if (!EditorPrefs.GetBool("reminded"))
+		 {
+			 Reminder.ShowWindow();
+		 }
+     }
+ }
+
+
